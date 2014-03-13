8bde2499e6143ccd5b0b9a2adb67709b3b718e61
Divit <dsingh_52@hotmail.com>
2014-03-12 22:06:52 -0400

    Implemented most of basic functionality, still needs dynamic listview and polishing

---
+++ src/com/collabhawk_android/ChatActivity.java
@@ -0,0 +1,153 @@
+package com.collabhawk_android;
+
+
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+
+public class ChatActivity extends Activity{
+	private EditText input_text;
+	private Button btn_Send;
+	private ListView listView;
+	private SocketListener socketListener;
+	SocketIO socket;
+	ArrayList<String> messages;
+	ArrayAdapter<String> adp;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_chat);
+		
+		initializeComponents();
+	}
+	
+	private void initializeComponents()
+	{
+		input_text = (EditText) findViewById(R.id.input_text);
+		btn_Send = (Button) findViewById(R.id.btn_Send);
+		listView = (ListView) findViewById(R.id.listView);
+		socketListener = new SocketListener();
+		messages = new ArrayList<String>();
+		btn_Send.setOnClickListener(socketListener);
+		adp = new ArrayAdapter<String> (getBaseContext(),
+				android.R.layout.simple_dropdown_item_1line,messages);
+		adp.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		listView.setAdapter(adp);
+		connectSockets();
+	}
+	
+	private void connectSockets()
+	{
+		try {
+			socket = new SocketIO("http://192.168.1.9:3000");
+			socket.connect(new IOCallback() {
+			    @Override
+			    public void on(String event, IOAcknowledge ack, Object... args) {
+			        if ("new_message".equals(event) && args.length > 0) {
+			            JSONObject jObject = (JSONObject) args[0];
+			            try {
+							messages.add(jObject.getString("Username") + " said: " + jObject.getString("Message"));
+							} catch (JSONException e) {
+							e.printStackTrace();
+						}
+			        } else if ("num_clients".equals(event) && args.length > 0) {
+			        } else if ("joined_room".equals(event) && args.length > 0) {
+			        	try {
+				        	JSONObject jObject = (JSONObject) args[0];
+							JSONArray jsonArgs = jObject.getJSONArray("messages");
+							for (int i = 0; i < jsonArgs.length(); i++)
+							{
+								JSONObject jMessage = jsonArgs.getJSONObject(i);
+								String userName = jMessage.getString("Username");
+								String message = jMessage.getString("said");
+								messages.add(userName + " said: " + message);
+							}
+				        	}catch (Exception e) {
+				        		e.printStackTrace();
+				        	}
+			        }
+			    }
+
+			    @Override
+			    public void onMessage(JSONObject json, IOAcknowledge ack) {}
+			    @Override
+			    public void onMessage(String data, IOAcknowledge ack) {}
+			    @Override
+			    public void onError(SocketIOException socketIOException) {}
+			    @Override
+			    public void onDisconnect() {}
+			    @Override
+			    public void onConnect() {}
+			});
+			
+			IOAcknowledge ack = new IOAcknowledge() {
+			    @Override
+			    public void ack(Object... args) {}
+			};
+			socket.emit("join_room", ack, getIntent().getExtras().getString("room"));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.search, menu);
+		return true;
+	}
+	
+	private class SocketListener implements OnClickListener
+	{
+
+		@Override
+		public void onClick(View v) {
+			String message = input_text.getText().toString();
+			if (message != null && message.length() > 0) 
+			{
+				IOAcknowledge ack = new IOAcknowledge() {
+				    @Override
+				    public void ack(Object... args) {}
+				};
+				JSONObject jObject = new JSONObject();
+				try {
+					jObject.put("Username", getIntent().getExtras().getString("Username"));
+					jObject.put("Message", message);
+					jObject.put("userKey", getIntent().getExtras().getString("id"));
+					jObject.put("room", getIntent().getExtras().getString("room"));
+					socket.emit("add_message", ack, jObject);
+					messages.add(getIntent().getExtras().getString("Username") + " said: " + message);
+					adp = new ArrayAdapter<String> (getBaseContext(),
+							android.R.layout.simple_dropdown_item_1line,messages);
+					adp.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+					listView.setAdapter(adp);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+}
---
+++ src/com/collabhawk_android/SearchActivity.java
@@ -0,0 +1,211 @@
+package com.collabhawk_android;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class SearchActivity extends Activity {
+	private EditText searchField;
+	private ListView searchView;
+	private Button searchButton;
+	private final String KEY="AIzaSyDJhytcECfUZ64UX-PqFPifGJc5gvrhppk";
+	private User loggedInUser;
+	private double lat, lon;
+	private LocationManager locationManager;
+	private ArrayList<String> placeNames;
+	private ArrayList<String> placeLocation;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search);
+		
+		initializeComponents();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.search, menu);
+		return true;
+	}
+	
+	private void initializeComponents() 
+	{
+		searchField = (EditText) findViewById(R.id.searchText);
+		searchView = (ListView) findViewById(R.id.searchView);
+		searchButton = (Button) findViewById(R.id.searchButton);
+		
+		loggedInUser = new User(getIntent().getStringExtra("Username"),
+				getIntent().getStringExtra("First_Name"),
+				getIntent().getStringExtra("Last_Name"),
+				getIntent().getStringExtra("Email"));
+	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    
+	    placeNames = new ArrayList<String>();
+		placeLocation  = new ArrayList<String>();
+		
+		searchView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				Intent i = new Intent(getApplicationContext(), ChatActivity.class);
+	            i.putExtra("Username", getIntent().getStringExtra("Username"));
+	            i.putExtra("Email", getIntent().getStringExtra("Email"));
+	            i.putExtra("First_Name", getIntent().getStringExtra("First_Name"));
+	            i.putExtra("Last_Name", getIntent().getStringExtra("Last_Name"));
+	            i.putExtra("id", getIntent().getStringExtra("id"));
+	            i.putExtra("room", arg0.getItemAtPosition(arg2).toString());
+	            startActivity(i);
+			}
+			
+		});
+		
+		searchButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			    Criteria criteria = new Criteria();
+			    Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+			    lat = location.getLatitude();
+			    lon = location.getLongitude();
+			    
+			    placeNames.removeAll(placeNames);
+			    placeLocation.removeAll(placeLocation);
+
+			    try {
+					String totalJson = new SearchAsyncTask().execute().get();
+					JSONObject jObject = new JSONObject(totalJson);
+					JSONArray results = jObject.getJSONArray("results");
+					
+					for (int i = 0; i < results.length(); i++)
+					{
+						placeNames.add(((JSONObject)results.get(i)).getString("name"));
+						placeLocation.add(((JSONObject)results.get(i)).getString("vicinity"));
+					}
+					
+					ArrayAdapter<String> adp=new ArrayAdapter<String> (getBaseContext(),
+							android.R.layout.simple_dropdown_item_1line,placeNames);
+					adp.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+					
+					searchView.setAdapter(adp);
+				   
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	    
+	}
+	
+	private void showToast(String message) 
+	{
+		Context context = getApplicationContext();
+    	int duration = Toast.LENGTH_SHORT;
+
+    	Toast toast = Toast.makeText(context, message, duration);
+    	toast.show();
+	}
+	
+	private class SearchAsyncTask extends AsyncTask<Void, Void, String>
+	{
+
+		@Override
+		protected String doInBackground(Void... arg0) {
+		    try {
+		    	HttpClient httpclient = new DefaultHttpClient();
+		        HttpGet request = new HttpGet();
+		        URI website = new URI(getParameters());
+		        request.setURI(website);
+		        String result = "";
+		        HttpResponse response = httpclient.execute(request);
+		        HttpEntity entity = response.getEntity();
+		        if (entity != null) 
+		        {
+		            // json is UTF-8 by default
+		            InputStream inputStream = entity.getContent();
+		            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+		            StringBuilder sb = new StringBuilder();
+	
+		            String line = null;
+		            while ((line = reader.readLine()) != null)
+		            {
+		                sb.append(line + "\n");
+		            }
+		            result = sb.toString();
+		            return result;
+		        }
+		    } catch (ClientProtocolException e) {
+		        // TODO Auto-generated catch block
+		    } catch (IOException e) {
+		        // TODO Auto-generated catch block
+		    } catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		private String getParameters() {
+			
+			String[] textArray = searchField.getText().toString().split(" ");
+			StringBuilder textArrayToSearch = new StringBuilder();
+			for (int i = 0; i < textArray.length; i++)
+			{
+				textArrayToSearch.append(textArray[i]);
+			}
+			StringBuilder builder = new StringBuilder();
+			builder.append("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+			builder.append("location="+ lat + "," + lon + "&");
+			builder.append("radius=50000&");
+			builder.append("rankBy=distance&");
+			builder.append("name=" + textArrayToSearch.toString() + "&");
+			builder.append("sensor=true&");
+			builder.append("key=" + KEY);
+			return builder.toString();
+		}
+		
+	}
+
+}
