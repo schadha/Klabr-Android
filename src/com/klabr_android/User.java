f3234af3549c97454f0d6b0dff036668fda4b1e5
Sanchit Chadha <schadha@vt.edu>
2014-04-10 05:36:32 -0400

    Initial Commit

---
+++ src/com/klabr_android/User.java
@@ -0,0 +1,13 @@
+package com.klabr_android;
+
+public class User {
+	private String userName, firstName, lastName, email;
+	
+	public User(String userName, String firstName, String lastName, String email)
+	{
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+}
